12f9c60ad4764caa90edacc39515a76c95622236
Alp Eren <erenalpturk@gmail.com>
2023-11-30 11:50:03 +0300

    fonts

--- src/app/components/TextTabComponent.jsx
+++ src/app/components/TextTabComponent.jsx
@@ -17,9 +17,9 @@ export default function TextTabComponent() {
             <div className='w-[90%] m-auto flex flex-col items-start gap-5 justify-center'>
                 <h1 className='text-3xl'>FONT STYLE</h1>
                 <div className='w-full flex flex-col gap-5 justify-start'>
-                    <MyButton text="Inter" click={() => { setFontFamily('font-Inter') }} cname={`font-Inter border ${inter && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
-                    <MyButton text="Roboto" click={() => { setFontFamily('font-roboto') }} cname={`font-roboto border ${roboto && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
-                    <MyButton text="Poppins" click={() => { setFontFamily('font-Poppins') }} cname={`font-Poppins border ${poppins && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
+                    <MyButton text="Inter" click={() => { setFontFamily('font-sans') }} cname={`font-sans border ${inter && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
+                    <MyButton text="Roboto" click={() => { setFontFamily('font-serif') }} cname={`font-serif border ${roboto && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
+                    <MyButton text="Poppins" click={() => { setFontFamily('font-mono') }} cname={`font-mono border ${poppins && 'border-blue-300 shadow-md shadow-blue-300'} text-start rounded py-2 pl-3`} />
                 </div>
             </div>
         </div>
